85bb45915460b29b75a39eaa8dc79fb45a0f4ff9
root07 <77151270+bekalhan@users.noreply.github.com>
2022-06-11 16:58:13 +0300

    initial commit

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
     console.log(`Server is running on PORT ${PORT}`);
